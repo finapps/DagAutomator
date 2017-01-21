5afb4ba8a025d31d1e46eefb6d09092e6d68d503
Christopher Rathnam <Rathnam.C@gmail.com>
2017-01-21 02:35:03 -0500

    Updated Menu Script

--- Programs/menu.py
+++ Programs/menu.py
@@ -1,25 +1,27 @@
 import runpy
-import os
+
 
 
 def menu():
-	print("Please choose a script to run")
-	print("1: Update Transaction Dates to Current Dates")
-	print("2: Randomize Transactions with in last 90 Days")
-	print("3: Exit")
+	print("\nDAG Automator \n Select a script to run \n 1. Checking Account \n 2. Investment Account \n 3. Randomize Checking Account \n 4. Exit")
 	choice = input()
 
 	if choice == "1":
 		file_globals = runpy.run_path("non_randomized_program.py")
-		print("Updating Transaction Dates")
+		print("Checking Account XML Generated")
 		menu()
 
 	if choice == "2":
+		#file = runpy.run_path("program_randomize.py")
+		print("Investment Account XML Generated")
+		menu()
+
+	if choice == "3":
 		file = runpy.run_path("program_randomize.py")
 		print("Generating Random XML")
 		menu()
 
-	if choice == "3":
+	if choice == "4":
 		print("Exiting...")
 		exit()
 menu()
