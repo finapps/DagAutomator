ac098acdff5680fb651437e86daaa2227916a8d2
pablok47 <pablo@financialapps.com>
2017-02-16 09:33:16 -0500

    udated the DagAutomator to include Fannie Mae Test Case Requirements

--- Master/AutoRunMenu.py
+++ Master/AutoRunMenu.py
@@ -4,5 +4,12 @@ file = runpy.run_path("checking_1_updater.py")
 file2 = runpy.run_path("checking_2_updater.py")
 file3 = runpy.run_path("securebank_1_updater.py")
 file4 = runpy.run_path("investment_1_updater.py")
-exit()	
-	
+file5 = runpy.run_path("fnma_checking_case1_updater.py")
+file6 = runpy.run_path("fnma_checking_case2_updater.py")
+file7 = runpy.run_path("fnma_checking_case3_updater.py")
+file8 = runpy.run_path("fnma_investment_case1_updater.py")
+file9 = runpy.run_path("fnma_investment_case2_updater.py")
+
+exit()
+
+# The AutoRunMenu includes regular dags plus fannie mae (fnma) dags.
---
+++ Master/fnma_checking_case1_updater.py
@@ -0,0 +1,97 @@
+from time import gmtime, strftime
+from datetime import datetime, timedelta, date as dt
+import dateutil.parser, dateutil.relativedelta
+import xml.etree.ElementTree as ET
+import random
+
+# Global Variables
+global updatedDate
+global dateDiff
+
+# Open File to be modified
+tree = ET.parse('fnma_checking_case1.xml')
+datesArray = []
+
+
+
+# Parser to convert date from ISOFormat to Date Object
+# This allows us to manipulate the date range.
+def getDateTimeFromISO8601String(i):
+  d = dateutil.parser.parse(i)
+  return d
+
+
+
+def oldDate(xmlFile):
+    transactions = tree.iter('transaction')
+    for transaction in transactions:
+        transDate = transaction.find('transDate').text
+        #print(transDate)
+        transactionDate = getDateTimeFromISO8601String(transDate)
+        #print(transactionDate)
+        datesArray.append(transactionDate)
+        #print(datesArray)
+        newArray = datesArray
+        #print(newArray)
+        #dateArray = list(newArray)
+    #print(dateArray)
+    return newArray
+
+def newDate(newArray):
+    newDateArray = []
+    for date in newArray:
+        #print(date)
+        youngest_date = max(newArray)
+        #print(youngest_date)
+        todayDate = datetime.now()
+        dateDiff = abs((todayDate - youngest_date).days)
+        #print(dateDiff)
+        newDate = date + dateutil.relativedelta.relativedelta(days=dateDiff)
+        #print(newDate)
+        date = str(newDate.isoformat())
+        newDateArray.append(date)
+        #print(newDateArray)
+    return newDateArray
+
+def updateXML(xmlFile):
+    #print(newDateArray)
+    #print(tree.findall('.//transDate')[0].text)
+    # for dateObj in tree.findall('.//transDate'): # for date in XMLtransDates
+    #     print(dateObj.text)
+    #     dateObj.text = newDateArray[dateObj]
+    #     print(dateObj)
+    #     break
+    #print(xmlFile)
+    originalDatesArr = oldDate(xmlFile)
+    #print(originalDatesArr)
+    adjustedDatesArr = newDate(originalDatesArr)
+    #print(adjustedDatesArr)
+    #transactions = xmlFile.iter('transDate')
+    #for transaction in transactions:
+    transDates = xmlFile.findall('.//transDate')
+    for num in range(0, len(transDates)):
+        #print(transDates[0])
+        #transDate = transaction.find('transDate')
+        #print("Original Value " + str(transDates[num].text))
+        #print("New Value " + str(adjustedDatesArr[num]))
+        transDates[num].text = adjustedDatesArr[num]
+        #print("Final Value " + str(transDates[num].text))
+
+
+    #Write back to a file
+    print("XML Generated")
+
+    now = datetime.now()
+    actual_time = str(now.strftime("%Y-%m-%d"))
+    xmlFile.write(str(actual_time) + "_fnma_checking_case1.xml", xml_declaration=True)
+
+    return None
+
+def testModule(dayDiff, youngest, today):
+    print ("\nToday's Date: " + str(today) + "\n")
+    print ("Most Recent Transaction Date: " + str(youngest) + "\n")
+    print ("Day Difference: " + str(dayDiff) + "\n")
+    return (dayDiff, youngest, today)
+
+#Update XMLFile
+updateXML(tree)
